68f0b8f974be80c32ddfb8036beeddaba3a4be6a
Dinesh Kumar Reddy C O <codinesh@live.com>
2015-08-06 13:57:06 +0530

    Added Map control

---
+++ Alarm/Alarm/Repository/GeoLocationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Alarm.Repository
+{
+    class GeoLocationRepository
+    {
+        public async Task<Geopoint> GetCurrentPosition()
+        {
+            return (await new Geolocator().GetGeopositionAsync()).Coordinate.Point;
+        }
+    }
+}
--- Alarm/Alarm/Views/LandingPage.xaml.cs
+++ Alarm/Alarm/Views/LandingPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 namespace Alarm.Views
 {
@@ -23,6 +25,40 @@ namespace Alarm.Views
         public LandingPage()
         {
             this.InitializeComponent();
+        }
+
+        public UIElement Marker
+        {
+            get
+            {
+                Canvas marker = new Canvas();
+                Ellipse outer = new Ellipse() { Width = 25, Height = 25 };
+                outer.Fill = new SolidColorBrush(Color.FromArgb(255, 240, 240, 240));
+                outer.Margin = new Thickness(-12.5, -12.5, 0, 0);
+                Ellipse inner = new Ellipse() { Width = 20, Height = 20 };
+                inner.Fill = new SolidColorBrush(Colors.Black);
+                inner.Margin = new Thickness(-10, -10, 0, 0);
+                Ellipse core = new Ellipse() { Width = 10, Height = 10 };
+                core.Fill = new SolidColorBrush(Colors.White);
+                core.Margin = new Thickness(-5, -5, 0, 0);
+                marker.Children.Add(outer);
+                marker.Children.Add(inner);
+                marker.Children.Add(core);
+                return marker;
+
+            }
+        }
+
+        private async void Location_Click(object sender, RoutedEventArgs e)
+        {
+            Repository.GeoLocationRepository locationAPI = new Repository.GeoLocationRepository();
+            var position = await locationAPI.GetCurrentPosition();
+
+            Windows.UI.Xaml.Controls.Maps.MapControl.SetLocation(Marker, position);
+            Windows.UI.Xaml.Controls.Maps.MapControl.SetNormalizedAnchorPoint(Marker, new Point(0.5, 0.5));
+            Map.ZoomLevel = 12;
+            Map.Center = position;
+            Map.MapServiceToken = "AtmksQlJbWwXX6wWzUNx2KRiYj_2PblvGPo0_RicXiM-HCb2epkj0sDAfwQl7hor";
         }
     }
 }
