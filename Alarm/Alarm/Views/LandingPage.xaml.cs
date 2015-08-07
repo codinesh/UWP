1ef33b388c4b71ae50d560182b5b69668b9e55c2
Dinesh Kumar <codinesh@live.com>
2015-08-07 14:37:59 +0530

    added basic map controls

--- Alarm/Alarm/Views/LandingPage.xaml.cs
+++ Alarm/Alarm/Views/LandingPage.xaml.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -22,43 +25,73 @@ namespace Alarm.Views
     /// </summary>
     public sealed partial class LandingPage : Page
     {
+        RandomAccessStreamReference mapIconStreamReference;
+
         public LandingPage()
         {
             this.InitializeComponent();
+            myMap.Loaded += MyMap_Loaded;
+            myMap.MapTapped += MyMap_MapTapped;
+            myMap.MapDoubleTapped += MyMap_MapDoubleTapped;
         }
 
-        public UIElement Marker
+        private void MyMap_MapDoubleTapped(MapControl sender, MapInputEventArgs args)
         {
-            get
-            {
-                Canvas marker = new Canvas();
-                Ellipse outer = new Ellipse() { Width = 25, Height = 25 };
-                outer.Fill = new SolidColorBrush(Color.FromArgb(255, 240, 240, 240));
-                outer.Margin = new Thickness(-12.5, -12.5, 0, 0);
-                Ellipse inner = new Ellipse() { Width = 20, Height = 20 };
-                inner.Fill = new SolidColorBrush(Colors.Black);
-                inner.Margin = new Thickness(-10, -10, 0, 0);
-                Ellipse core = new Ellipse() { Width = 10, Height = 10 };
-                core.Fill = new SolidColorBrush(Colors.White);
-                core.Margin = new Thickness(-5, -5, 0, 0);
-                marker.Children.Add(outer);
-                marker.Children.Add(inner);
-                marker.Children.Add(core);
-                return marker;
-
-            }
+            myMap.MapElements.RemoveAt(myMap.MapElements.Count);
         }
 
-        private async void Location_Click(object sender, RoutedEventArgs e)
+        private async void MyMap_Loaded(object sender, RoutedEventArgs e)
         {
             Repository.GeoLocationRepository locationAPI = new Repository.GeoLocationRepository();
             var position = await locationAPI.GetCurrentPosition();
+            myMap.Center = new Geopoint(position.Position);
+            myMap.ZoomLevel = 12;
+        }
 
-            Windows.UI.Xaml.Controls.Maps.MapControl.SetLocation(Marker, position);
-            Windows.UI.Xaml.Controls.Maps.MapControl.SetNormalizedAnchorPoint(Marker, new Point(0.5, 0.5));
-            Map.ZoomLevel = 12;
-            Map.Center = position;
-            Map.MapServiceToken = "AtmksQlJbWwXX6wWzUNx2KRiYj_2PblvGPo0_RicXiM-HCb2epkj0sDAfwQl7hor";
+        private void MyMap_MapTapped(Windows.UI.Xaml.Controls.Maps.MapControl sender, Windows.UI.Xaml.Controls.Maps.MapInputEventArgs args)
+        {
+            var tappedGeoPosition = args.Location.Position;
+            string status = "MapTapped at \nLatitude:" + tappedGeoPosition.Latitude + "\nLongitude: " + tappedGeoPosition.Longitude;
+
+            AddMapIcon(args.Location, tappedGeoPosition.Latitude.ToString());
+        }
+
+        private void TrafficFlowVisible_Checked(object sender, RoutedEventArgs e)
+        {
+            myMap.TrafficFlowVisible = true;
+        }
+
+        private void trafficFlowVisibleCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            myMap.TrafficFlowVisible = false;
+        }
+
+        private void btnAddMapIcon_Click(object sender, RoutedEventArgs e)
+        {
+            AddMapIcon(myMap.Center, myMap.Center.GeoshapeType.ToString());
+        }
+
+        private void btnClearMapIcons_Click(object sender, RoutedEventArgs e)
+        {
+            myMap.MapElements.Clear();
+        }
+
+        private async void btnCurrentLocation_Click(object sender, RoutedEventArgs e)
+        {
+            Repository.GeoLocationRepository locationAPI = new Repository.GeoLocationRepository();
+            var position = await locationAPI.GetCurrentPosition();
+            myMap.Center = new Geopoint(position.Position);
+            myMap.ZoomLevel = 18;
+        }
+
+        private void AddMapIcon(Geopoint position, string title) {
+            MapIcon mapIcon1 = new MapIcon();
+            mapIcon1.Location = position;
+            mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            mapIcon1.Title = title;
+            mapIcon1.Image = mapIconStreamReference;
+            mapIcon1.ZIndex = 0;
+            myMap.MapElements.Add(mapIcon1);
         }
     }
 }
